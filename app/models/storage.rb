f0c4a6071922372b470d0f3d9d85e5c06a1a9fa2
Pavel Novitskiy <altusensix@gmail.com>
2013-05-02 09:28:24 +0400

    Add whenever gem to periodically remove old passwords.

--- app/models/storage.rb
+++ app/models/storage.rb
@@ -2,4 +2,10 @@ class Storage < ActiveRecord::Base
   attr_accessible :key, :password
   validates :password, :key, :presence => true
   validates_length_of :password, :maximum => 255
+
+  def self.remove_old
+    # Remove passwords older than month
+    Storage.where(:created_at < (Time.now-(60*60*24*30))).destroy_all
+  end
+
 end
